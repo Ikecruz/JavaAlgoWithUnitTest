82eb035ae02c1215595e744915074e185c9b0a70
Adebayo Michael <mcgamma04@yahoo.com>
2022-05-15 21:00:17 +0100

    Update GradeBook.java
    Create a character array and store the grade letter in it.

--- src/main/java/com/algo/GradeBook.java
+++ src/main/java/com/algo/GradeBook.java
@@ -8,6 +8,8 @@ public class GradeBook {
         char grade = ' ';
 
         average = (s1 + s2 + s3) / 3;
+        //try to use the methiod below to store grade letter in an array. It will be clearner and easy to read
+       // char[] gr = new char[]{'A','B','C','D','F'}
 
         if ((90 <= average) && ( average <= 100)) grade = 'A';
         else if ((80 <= average) && ( average < 90)) grade = 'B';
